c3fe1a479513fd30ebf58336a6654d353f32f273
Trent Mentink <trentmentink@gmail.com>
2019-02-28 17:01:13 -0800

    testing cors

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const port = process.env.PORT || 4000;
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({
-  noCors: true
-})
+const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use(router)
